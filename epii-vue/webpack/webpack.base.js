635414ad1cad874be247e2b154650f776c1632ef
Your Name <you@example.com>
2022-11-03 14:20:48 +0800

    rlr

--- epii-vue/webpack/webpack.base.js
+++ epii-vue/webpack/webpack.base.js
@@ -20,14 +20,14 @@ const pagesManger = require("../tools/pages_manager.js").getPages();
 
 const buildType = require("../tools/build_type.js");
 let config_base_file = path.resolve(work_dir + "/config/config.base.js");
-let  build_config = {remUnit:75};
-if(fs.existsSync(config_base_file)){
-    build_config = Object.assign(build_config,require(config_base_file));
+let build_config = { remUnit: 75 };
+if (fs.existsSync(config_base_file)) {
+    build_config = Object.assign(build_config, require(config_base_file));
 }
 
- 
+
 let config = merger({
-    devtool:false,
+    devtool: false,
     entry: pagesManger.entries,
     output: {
         filename: "js/[name].[chunkhash:8].js",
@@ -38,71 +38,80 @@ let config = merger({
 
     module: {
         rules: [{
-                test: /\.vue$/,
-                use: ['vue-loader']
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', {//'style-loader'
-                    loader: 'px2rem-loader',
-                    options: {
-                        remUnit: build_config.remUnit,
-                        remPrecision: 8
-                    }
-                }],
-            },
-            {
-                // 打包图片
-                test: /\.(jpe?g|png|gif)$/,
-                use: {
-                    // loader: 'file-loader',
-                    loader: 'url-loader',
-                    options: {
-                        name: '[name]_[hash].[ext]',
-                        outputPath: 'images/',
-                        limit: 1,
-                        esModule: false,
-                    },
+            test: /\.vue$/,
+            use: ['vue-loader']
+        },
+        {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', {//'style-loader'
+                loader: 'px2rem-loader',
+                options: {
+                    remUnit: build_config.remUnit,
+                    remPrecision: 8
+                }
+            }],
+        },
+        {
+            // 打包图片
+            test: /\.(jpe?g|png|gif)$/,
+            use: {
+                // loader: 'file-loader',
+                loader: 'url-loader',
+                options: {
+                    name: '[name]_[hash].[ext]',
+                    outputPath: 'images/',
+                    limit: 1,
+                    esModule: false,
                 },
-                type: 'javascript/auto'
             },
-            { // 处理字体
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'font/',
-                    },
+            type: 'javascript/auto'
+        },
+        { // 处理字体
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'font/',
                 },
+            },
 
-            }, {
-                test: /\.svg/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'px2rem-loader',
-                        options: {
-                            remUnit: build_config.remUnit,
-                            remPrecision: 8
-                        }
-                    },'less-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', {
+        }, {
+            test: /\.svg/,
+            use: ['file-loader']
+        },
+        {
+            test: /\.less$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                {
                     loader: 'px2rem-loader',
                     options: {
                         remUnit: build_config.remUnit,
                         remPrecision: 8
                     }
-                },'sass-loader']
+                }, 'less-loader'
+            ]
+        },
+        {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', {
+                loader: 'px2rem-loader',
+                options: {
+                    remUnit: build_config.remUnit,
+                    remPrecision: 8
+                }
+            }, 'sass-loader']
+        },
+        {
+            test: /\.(mp3|mp4)$/,
+            loader: 'url-loader',
+            options: {
+                name: 'audios/[name].[ext]',
+                limit: 10,
+                esModule: false,
             }
+        }
 
         ]
     },
